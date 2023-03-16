70d41c4aee480e15559a88127746aa5725113df9
ItayMizrachi <itaymiz98@gmail.com>
2023-03-16 09:33:49 +0200

    start

---
+++ src/components/AppRouters.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./Home";
+import Page404 from "./Page404";
+
+export default function AppRouters() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/*" element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
